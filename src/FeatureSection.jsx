50f48cab2b7cb9968107a6216337671a9ec3d79a
suriya-a9 <uriyaprakash@gmail.com>
2025-10-22 12:32:53 +0530

    Changes

--- src/FeatureSection.jsx
+++ src/FeatureSection.jsx
@@ -146,7 +146,7 @@ const FeatureSection = () => {
                                 src={activeFeature.image}
                                 alt={activeFeature.title}
                                 style={{ width: '100%', objectFit: 'cover', borderRadius: '16px' }}
-                                className='filter grayscale'
+                                // className='filter grayscale'
                             />
                         </div>
                         <div style={{ marginTop: '15px' }}>
@@ -245,7 +245,7 @@ const FeatureSection = () => {
                                 boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
 
                             }}
-                            className='filter grayscale'
+                            // className='filter grayscale'
                         />
                         <div
                             style={{
